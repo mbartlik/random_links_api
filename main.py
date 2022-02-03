ae87acc0b07c202c8913495e9f1d10b1c112850d
MLHLocalhost <localhost@mlh.io>
2022-02-03 17:35:56 -0500

    Added post functionality

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 from os import path
 import pymysql
 import random
+import requests
 
 from flask import Flask
 from flask import jsonify
+from flask import request
+
 
 
 app = Flask(__name__)
@@ -17,7 +20,7 @@ db_password = ''
 db_name = 'main'
 db_connection_name = 'random-links-api:us-east4:random-links'
 
-deployed = True
+deployed = False
 
 # Establishes connection with Google Cloud SQL database
 def get_connection():
@@ -34,83 +37,162 @@ def get_connection():
 
 	return conn
 
-@app.route("/fun-link")
+@app.route("/fun-link", methods=['GET', 'POST'])
 def fun_link():
     # get connection and cursor
     conn = get_connection()
     cur = conn.cursor()
 
-    # get links from database
-    cur.execute('SELECT * FROM fun_links')
-    fun_links = cur.fetchall()
+    if request.method == 'GET':
+        # get links from database
+        cur.execute('SELECT * FROM fun_links')
+        fun_links = cur.fetchall()
 
-    # if there are no links in the category return relevant response
-    if len(fun_links) == 0:
+        # if there are no links in the category return relevant response
+        if len(fun_links) == 0:
+            return jsonify(
+                link='null',
+                description='there are no links in the database'
+            ), 204
+
+        # pick random link from the list
+        rand_choice = random.randrange(len(fun_links))
+        rand_link = fun_links[rand_choice][0]
+        rand_description = fun_links[rand_choice][1]
+
         return jsonify(
-            link='null',
-            description='there are no links in the database'
-        ), 204
+            link=rand_link,
+            description=rand_description
+        ), 200
+    if request.method == 'POST':
+        new_link = request.form['link']
+        new_description = request.form['description']
 
-    # pick random link from the list
-    rand_choice = random.randrange(len(fun_links))
-    rand_link = fun_links[rand_choice][0]
-    rand_description = fun_links[rand_choice][1]
+        try:
+            requests.get(new_link)
+        except Exception as e:
+            return jsonify(
+                status="Failure - " + new_link + " is an invalid url"
+            ), 406
 
-    return jsonify(
-        link=rand_link,
-        description=rand_description
-    ), 200
+        try:
+            cur.execute('INSERT INTO fun_links VALUES (%s, %s)', (new_link, new_description))
+            conn.commit()
+            conn.close()
 
-@app.route("/game-link")
+            return jsonify(
+                status="Success - Added " + new_link + " to fun links"
+            )
+        except:
+            pass
+        return jsonify(
+            status="Failure - an error occurred when adding " + new_link + " to fun links"
+        )
+            
+
+        
+
+
+@app.route("/game-link", methods=['GET', 'POST'])
 def game_link():
     # get connection and cursor
     conn = get_connection()
     cur = conn.cursor()
 
-    # get links from database
-    cur.execute('SELECT * FROM game_links')
-    game_links = cur.fetchall()
+    if request.method == 'GET':
+        # get links from database
+        cur.execute('SELECT * FROM game_links')
+        game_links = cur.fetchall()
 
-    # if there are no links in the category return relevant response
-    if len(game_links) == 0:
+        # if there are no links in the category return relevant response
+        if len(game_links) == 0:
+            return jsonify(
+                link='null',
+                description='there are no links in the database'
+            ), 204
+
+        # pick random link from the list
+        rand_choice = random.randrange(len(game_links))
+        rand_link = game_links[rand_choice][0]
+        rand_description = game_links[rand_choice][1]
+
         return jsonify(
-            link='null',
-            description='there are no links in the database'
-        ), 204
+            link=rand_link,
+            description=rand_description
+        ), 200
+    if request.method == 'POST':
+        new_link = request.form['link']
+        new_description = request.form['description']
 
-    # pick random link from the list
-    rand_choice = random.randrange(len(game_links))
-    rand_link = game_links[rand_choice][0]
-    rand_description = game_links[rand_choice][1]
+        try:
+            requests.get(new_link)
+        except Exception as e:
+            return jsonify(
+                status="Failure - " + new_link + " is an invalid url"
+            ), 406
 
-    return jsonify(
-        link=rand_link,
-        description=rand_description
-    ), 200
+        try:
+            cur.execute('INSERT INTO game_links VALUES (%s, %s)', (new_link, new_description))
+            conn.commit()
+            conn.close()
+            
+            return jsonify(
+                status="Success - Added " + new_link + " to game links"
+            )
+        except:
+            pass
+        return jsonify(
+            status="Failure - an error occurred when adding " + new_link + " to game links"
+        )
 
-@app.route("/news-link")
+@app.route("/news-link", methods=['GET', 'POST'])
 def news_link():
     # get connection and cursor
     conn = get_connection()
     cur = conn.cursor()
 
-    # get links from database
-    cur.execute('SELECT * FROM news_links')
-    news_links = cur.fetchall()
+    if request.method == 'GET':
+        # get links from database
+        cur.execute('SELECT * FROM news_links')
+        news_links = cur.fetchall()
 
-    # if there are no links in the category return relevant response
-    if len(news_links) == 0:
+        # if there are no links in the category return relevant response
+        if len(news_links) == 0:
+            return jsonify(
+                link='null',
+                description='there are no links in the database'
+            ), 204
+
+        # pick random link from the list
+        rand_choice = random.randrange(len(news_links))
+        rand_link = news_links[rand_choice][0]
+        rand_description = news_links[rand_choice][1]
+
         return jsonify(
-            link='null',
-            description='there are no links in the database'
-        ), 204
+            link=rand_link,
+            description=rand_description
+        ), 200
+    if request.method == 'POST':
+        new_link = request.form['link']
+        new_description = request.form['description']
 
-    # pick random link from the list
-    rand_choice = random.randrange(len(news_links))
-    rand_link = news_links[rand_choice][0]
-    rand_description = news_links[rand_choice][1]
+        try:
+            requests.get(new_link)
+        except Exception as e:
+            return jsonify(
+                status="Failure - " + new_link + " is an invalid url"
+            ), 406
 
-    return jsonify(
-        link=rand_link,
-        description=rand_description
-    ), 200
+        try:
+            cur.execute('INSERT INTO news_links VALUES (%s, %s)', (new_link, new_description))
+            conn.commit()
+            conn.close()
+            
+            return jsonify(
+                status="Success - Added " + new_link + " to news links"
+            )
+        except:
+            pass
+        return jsonify(
+            status="Failure - an error occurred when adding " + new_link + " to news links"
+        )
